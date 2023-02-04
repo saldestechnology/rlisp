522cf3ec0b303d088d5ea046e2efb9b06e59e9cf
Johan Saldes <johan@saldestechnology.com>
2023-02-04 12:55:02 +0100

    started experimenting with stdin and cursor

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
+use std::io::{self, stdin, Cursor, Write};
+
+use crate::lexer::tokenize;
+
 mod lexer;
 
 fn main() {
-    println!("Hello, world!");
+    let buf = String::new();
+    let mut cursor = Cursor::new(buf);
+    loop {
+        cursor.get_mut().clear();
+        print!("rlisp> ");
+        let _ = io::stdout().flush();
+        match stdin().read_line(cursor.get_mut()) {
+            Ok(i) => println!("output: {:?}", tokenize(cursor.get_ref().as_str())),
+            Err(e) => println!("Error: {}", e),
+        }
+    }
 }
